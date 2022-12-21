2aa1d8542427dd3575ee1a5379fd2311e993a7b0
abdul kareem <abdulak4t7@gmail.com>
2022-12-21 11:18:35 -0800

    customising the portofolio

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,11 @@ import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {
   AiFillTwitterCircle,
   AiFillLinkedin,
+  AiFillMail,
 } from 'react-icons/ai'
 import { useState } from 'react';
 import Image from 'next/image';
-import deved from '../public/dev-ed-wave.png';
+import avatar from '../public/avatar.jpg';
 import code from "../public/code.png";
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
@@ -56,18 +57,18 @@ export default function Home() {
               Abdul Kareem 
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+            React Frontend Web Developer
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
+            I develope web applications using React and other modern technologies. I am a computer science student and I am passionate for creating intuitive and user-friendly web experiences.
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <AiFillTwitterCircle />
               <AiFillLinkedin />
+              <AiFillMail />
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" />
+              <Image src={avatar} layout="fill" objectFit="cover" />
             </div>
           </div>
         </section>

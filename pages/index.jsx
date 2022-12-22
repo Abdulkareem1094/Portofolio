4a2b4b9780df8bd61f4471a9ec820b64b231e0ca
abdul kareem <abdulak4t7@gmail.com>
2022-12-22 04:52:58 -0800

    did a few tweeks

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,13 +4,11 @@ import {
   AiFillTwitterCircle,
   AiFillLinkedin,
   AiFillMail,
+  AiFillGithub,
 } from 'react-icons/ai'
 import { useState } from 'react';
 import Image from 'next/image';
-import avatar from '../public/avatar.jpg';
 import code from "../public/code.png";
-import design from "../public/design.png";
-import consulting from "../public/consulting.png";
 import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
 import web3 from "../public/web3.png";
@@ -20,7 +18,7 @@ import web6 from "../public/web6.png";
 
 export default function Home() {
 
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(true)
 
   return (
     <div className={ darkMode ? 'dark' : ''} >
@@ -63,82 +61,52 @@ export default function Home() {
             I develope web applications using React and other modern technologies. I am a computer science student and I am passionate for creating intuitive and user-friendly web experiences.
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <AiFillTwitterCircle />
-              <AiFillLinkedin />
-              <AiFillMail />
-            </div>
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={avatar} layout="fill" objectFit="cover" />
+              <AiFillTwitterCircle className=' cursor-pointer '/>
+              <AiFillGithub className=' cursor-pointer '/>
+              <AiFillLinkedin className=' cursor-pointer '/>
+              <AiFillMail className=' cursor-pointer '/>
             </div>
           </div>
         </section>
+
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <h3 className="text-3xl py-1 dark:text-white ">Skills</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
+              <span className="text-teal-500"> Strong understanding </span>
+              of HTML, CSS, JavaScript and React
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+              <span className="text-teal-500"> Proficient </span>
+              in using Git for version control, Figma, Redux, GraphQL, and Apollo
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              <span className="text-teal-500"> Experience </span>
+              with responsive web design and mobile-first development
             </p>
           </div>
-          <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image className='mx-auto' src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
-              </h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
+          <div className="pt-20">
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image className='mx-auto' src={code} width={100} height={100} />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Code your dream project
               </h3>
               <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
+              Are you ready to take your online presence to the next level and create a website that truly represents your brand or business?
+              If you have an idea for your next great website, I'd love to help make it a reality. Together, we can turn your vision into a functional,
+              visually appealing, and user-friendly website that will attract and retain visitors. Let's start turning your dream website into a reality.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image className='mx-auto' src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
             </div>
           </div>
         </section>
 
         <section>
+          <h3 className="text-5xl py-5 pt-20 text-center text-teal-600 font-medium dark:text-teal-400 ">
+              Projects
+          </h3>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-            <div className='basis-1/3 flex-1'>
-              <Image src={web1} className='rounded-lg object-cover' width={'100%'} height={'100%'}  />
+            <div className='basis-1/4 flex-1'>
+              <Image src={web1} className='hover:bg-teal-800 bg-opacity-5 rounded-lg object-cover' width={'100%'} height={'100%'}  />
             </div>
             <div className='basis-1/3 flex-1'>
               <Image src={web2} className='rounded-lg object-cover' width={'100%'} height={'100%'}  />

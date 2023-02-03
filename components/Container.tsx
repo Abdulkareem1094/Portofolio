06255c7d15a23807f3b7d3169b0fbfdc731f4056
abdul kareem <abdulak4t7@gmail.com>
2023-02-03 06:41:18 -0800

    redoing my portofolio

---
+++ components/Container.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div className='w-full h-screen border-gray-700 bg-gray-700 opacity-30 ' >
+         
+    </div>
+  )
+}
+export default Container
